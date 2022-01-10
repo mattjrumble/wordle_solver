98d664e643b9e0134688ca6a08e41b3f4b07bb04
mattjrumble <mattjrumble@gmail.com>
2022-01-10 19:32:58 +0000

    Improve second guesses to favour words that might actually be correct

--- wordle_solver/strategy_3/compute_best_second_guess.py
+++ wordle_solver/strategy_3/compute_best_second_guess.py
@@ -13,7 +13,7 @@ class WorseTotal(Exception):
     pass
 
 
-def check_for_better_total(words_after_first_guess, second_guess, current_best_total):
+def check_for_better_total(words_after_first_guess, second_guess, current_best_total=None):
     """
     Calculate the total number of remaining words after the given second guess, summed across all possible answers.
     Raise a `WorseAverage` exception if this number is larger than the current best total.
@@ -30,7 +30,7 @@ def check_for_better_total(words_after_first_guess, second_guess, current_best_t
             new_remaining_words = words_remaining_for_given_result(guess=second_guess, result=result)
             result_to_remaining_words_cache[result] = new_remaining_words
         total += len(words_after_first_guess.intersection(new_remaining_words))
-        if total > current_best_total:
+        if current_best_total and total > current_best_total:
             raise WorseTotal()
     return total
 
@@ -41,6 +41,7 @@ def main():
     number of remaining possible words. Store this mapping of {result -> best second guess} in a file.
     """
     possible_results = sorted(set(result_of_guess(guess=FIRST_GUESS, answer=word) for word in WORDS))
+    possible_results.remove((2, 2, 2, 2, 2))  # If the first guess was correct, there is no second guess.
     for result in possible_results:
         print(f'Calculating for result: {result}')
         words_after_first_guess = words_remaining_for_given_result(guess=FIRST_GUESS, result=result)
@@ -59,11 +60,20 @@ def main():
             else:
                 best_second_guess = word
                 best_total = total
+
                 # If the total is equal to the length of possible answers, then this second guess guarantees the word
-                # can be found next round (if it hasn't been found already). We can't do any better than this, so take
-                # this word as the best second guess for this particular result.
+                # can be found next round (if it hasn't been found already). The only way we can improve on this is
+                # by finding a second guess meets this condition whilst also being in the list of remaining possible
+                # words - so that this second guess has a chance of being correct.
                 if best_total == len(words_after_first_guess):
+                    for remaining_word in words_after_first_guess:
+                        if check_for_better_total(
+                            words_after_first_guess=words_after_first_guess, second_guess=remaining_word
+                        ) == len(words_after_first_guess):
+                            best_second_guess = remaining_word
+                            break
                     break
+
         print(
             f'{result} - Best second guess is {best_second_guess} '
             f'with {best_total / len(words_after_first_guess)} average remaining words '
