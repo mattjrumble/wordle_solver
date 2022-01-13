c387553e3e50bfb2caccbf6f24bc86b917bab0f8
mattjrumble <mattjrumble@gmail.com>
2022-01-13 19:05:24 +0000

    Move LETTER_FREQUENCIES into the Strategy 2 directory

--- wordle_solver/__init__.py
+++ wordle_solver/__init__.py
@@ -2,13 +2,6 @@ with open('words.txt') as fd:
     WORDS = [line.strip() for line in fd.readlines()]
 
 
-LETTER_FREQUENCIES = {
-    'A': 5738, 'B': 1547, 'C': 1947, 'D': 2364, 'E': 6441, 'F': 1071, 'G': 1576, 'H': 1682, 'I': 3595, 'J': 270,
-    'K': 1426, 'L': 3269, 'M': 1908, 'N': 2854, 'O': 4248, 'P': 1945, 'Q': 104, 'R': 4027, 'S': 6427, 'T': 3206,
-    'U': 2399, 'V': 661, 'W': 1013, 'X': 268, 'Y': 1992, 'Z': 412
-}
-
-
 def result_of_guess(guess, answer):
     """
     Return a tuple of five integers representing the result of a guess.
